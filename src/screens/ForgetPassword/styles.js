6c6eb0bad96adb99abde4cc5beda5bf8e77cebae
souravwebsadroit <souravwebsadroit@gmail.com>
2023-06-20 12:16:56 +0530

    forgetPassword & fpotp validation

--- src/screens/ForgetPassword/styles.js
+++ src/screens/ForgetPassword/styles.js
@@ -81,6 +81,29 @@ export default StyleSheet.create({
             textAlign: 'center',
             fontFamily: fontFamily.regular,
         }
-    }
+    },
+    errorText: {
+        color: 'red',
+        fontSize: fontSize.regular,
+        fontFamily: fontFamily.regular,
+        marginTop: 5,
+      },
+      errorInput: {
+        borderColor: 'red',
+      },
+      textInput: {
+        color: BKColor.textColor1,
+        fontFamily: fontFamily.regular,
+        fontSize: fontSize.h3,
+        borderRadius: 30,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: BKColor.inputBorder,
+        padding: hp('1.5%'),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: hp('6.5%'),
+      },
 
 });
